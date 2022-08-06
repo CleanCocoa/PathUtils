164999c207124e515992dadd8105e57edcc57866
Christian Tietze <me@christiantietze.de>
2022-08-06 12:27:27 +0200

    add Basename from The Archive

---
+++ Sources/Filename/Basename.swift
@@ -0,0 +1,45 @@
+//  Copyright © 2022 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import Foundation
+
+/// Representation of a filename with its path extension. So "/path/to/foo.txt" becomes "foo.txt".
+///
+/// See ``Filename`` for filename without path extension.
+public struct Basename: Equatable, Hashable {
+    public let filename: Filename
+    /// When the file has no path extension, an empty string.
+    public let pathExtension: String
+
+    public init(filename: Filename,
+                pathExtension: String) {
+        self.filename = filename
+        self.pathExtension = pathExtension
+    }
+
+    public init?(url: URL) {
+        guard let filename = Filename(url: url) else { return nil}
+        self.init(filename: filename,
+                  pathExtension: url.pathExtension)
+    }
+
+    public func url(relativeTo url: URL) -> URL {
+        return filename.url(
+           relativeTo: url,
+           pathExtension: self.pathExtension)
+    }
+
+    public func url(relativeTo folder: Folder) -> URL {
+        return filename.url(
+            relativeTo: folder,
+            pathExtension: pathExtension)
+    }
+}
+
+extension Basename: CustomStringConvertible {
+    public var description: String {
+        let suffix = pathExtension.isEmpty
+            ? ""
+            : ".\(pathExtension)"
+        return "Basename(\(filename.string)\(suffix))"
+    }
+}
