b4884227bf9053710a0e1876c7d186f18615d670
Christian Tietze <me@christiantietze.de>
2022-08-06 12:07:52 +0200

    add Folder type from The Archive

---
+++ Sources/Filename/Folder.swift
@@ -0,0 +1,22 @@
+//  Copyright © 2017 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import Foundation
+
+/// - Note: Equatability does compare `absoluteString` of `url`, not URLs themselves, for better compatibility between different ways to point to the same file.
+public struct Folder: Equatable, CustomStringConvertible {
+    public let url: URL
+
+    public init?(url: URL) {
+        guard url.hasDirectoryPath else { return nil }
+        self.url = url
+    }
+
+    public var description: String {
+        return "Folder(path=\"\(url.path)\")"
+    }
+
+    public static func == (lhs: Folder, rhs: Folder) -> Bool {
+        // We compare computed paths, not URL objects, so that `URL(fileURLWithPath: "/foo/bar")` and `URL(fileURLWithPath: "bar", relativeTo: URL(fileURLWithPath: "/foo"))` are treated equally. Same with relative file paths involving `../`.
+        return lhs.url.absoluteString == rhs.url.absoluteString
+    }
+}
