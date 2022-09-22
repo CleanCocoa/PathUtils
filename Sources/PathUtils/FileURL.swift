7b9748d43cc78362c986d246625ccfcc7df7c209
Christian Tietze <me@christiantietze.de>
2022-09-22 16:05:24 +0200

    add FileURL to wrap the "is a file URL" check

--- Sources/PathUtils/FileURL.swift
+++ Sources/PathUtils/FileURL.swift
@@ -1,8 +1,32 @@
-//
-//  File.swift
-//  
-//
-//  Created by Christian Tietze on 22.09.22.
-//
+//  Copyright © 2022 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
-import Foundation
+import struct Foundation.URL
+
+extension URL {
+    /// ``FileURL``, if the receiver is pointing to a resource on the local file system.
+    @inlinable
+    public var fileURL: FileURL? { FileURL(from: self) }
+}
+
+/// A URL that points to a resource on the local file system, i.e. via the `file:` scheme. 
+/// - Invariant: Cannot be constructed for non-file-URLs. Adheres to the same rules as  ``URL.isFileURL``.
+public struct FileURL: Equatable {
+    public let url: URL
+
+    /// Filename of the file without path extension, so "/path/to/foo.txt" becomes "foo".
+    public var filename: Filename? { Filename(url: url) }
+
+    /// Name of the file with path extension, so “/path/to/foo.txt” becomes “foo.txt”.
+    public var basename: Basename? { Basename(url: url) }
+
+    /// Folder representation of the `url`'s parent path component.
+    public var folder: Folder? {
+        Folder(url: url.deletingLastPathComponent()) }
+
+    public init?(from url: URL) {
+        guard url.isFileURL,
+              !url.hasDirectoryPath
+        else { return nil }
+        self.url = url
+    }
+}
--- Tests/PathUtilsTests/FileURLTests.swift
+++ Tests/PathUtilsTests/FileURLTests.swift
@@ -1,8 +1,59 @@
-//
-//  File.swift
-//  
-//
-//  Created by Christian Tietze on 22.09.22.
-//
+//  Copyright © 2022 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
-import Foundation
+import XCTest
+@testable import PathUtils
+
+class FileURLTests: XCTestCase {
+    let webURL = URL(string: "http://web.addres.se")!
+    let fileURL = URL(string: "file:///tmp/foo.bar")!
+    let folderURL = URL(fileURLWithPath: "/xyz", isDirectory: true)
+    let relativeFileURL = URL(
+        fileURLWithPath: "subdir/file.txt",
+        relativeTo: URL(fileURLWithPath: "/tmp/relative/"))
+    let rootURL = URL(fileURLWithPath: "/")
+
+    func testInitializer() {
+        XCTAssertNil(FileURL(from: webURL))
+        XCTAssertNil(FileURL(from: folderURL))
+        XCTAssertNil(FileURL(from: rootURL))
+        XCTAssertNotNil(FileURL(from: fileURL))
+        XCTAssertNotNil(FileURL(from: relativeFileURL))
+    }
+
+    func testFilename() {
+        XCTAssertEqual(FileURL(from: fileURL)?.filename,
+                       Filename("foo"))
+        XCTAssertEqual(FileURL(from: relativeFileURL)?.filename,
+                       Filename("file"))
+    }
+
+    func testBasename() {
+        XCTAssertEqual(FileURL(from: fileURL)?.basename,
+                       Basename(filename: Filename("foo"),
+                                pathExtension: "bar"))
+        XCTAssertEqual(FileURL(from: relativeFileURL)?.basename,
+                       Basename(filename: Filename("file"),
+                               pathExtension: "txt"))
+    }
+
+    func testFolder_FilesInDirectories() {
+        XCTAssertEqual(FileURL(from: fileURL)?.folder?.path,
+                       "/tmp")
+        XCTAssertEqual(FileURL(from: relativeFileURL)?.folder?.path,
+                       "/tmp/relative/subdir")
+
+        let relativeFileURLWithoutSubdir = URL(
+            fileURLWithPath: "file.txt",
+            relativeTo: URL(fileURLWithPath: "/tmp/relative/"))
+        XCTAssertEqual(FileURL(from: relativeFileURLWithoutSubdir)?.folder?.path,
+                       "/tmp/relative")
+    }
+
+    func testFolder_RootLevelFiles() throws {
+        let rootFolder = try XCTUnwrap(Folder(url: URL(fileURLWithPath: "/", isDirectory: true)))
+
+        let rootFile = URL(fileURLWithPath: "/root.xyz")
+        XCTAssertEqual(FileURL(from: rootFile)?.folder,
+                       rootFolder)
+    }
+}
