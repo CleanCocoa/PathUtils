1dca8064ed3a7e82d2f0a58af3a5b2f54deb6723
Christian Tietze <me@christiantietze.de>
2024-11-23 15:16:36 +0100

    make ContentfulString inlinable

--- Sources/PathUtils/ContentfulString.swift
+++ Sources/PathUtils/ContentfulString.swift
@@ -2,15 +2,18 @@
 
 import Foundation  // for String.contains(_:) that takes another String
 
+@inlinable
 public func contentful(_ string: String?) -> ContentfulString? {
     guard let string = string else { return nil }
     return ContentfulString(string)
 }
 
+@inlinable
 public func contentful(_ string: String) -> ContentfulString? {
     return ContentfulString(string)
 }
 
+@inlinable
 public func contentful(_ strings: [String]?) -> [ContentfulString] {
     guard let strings = strings else { return [] }
     return strings.compactMap(contentful)
@@ -19,6 +22,7 @@ public func contentful(_ strings: [String]?) -> [ContentfulString] {
 public struct ContentfulString: Equatable, Hashable, Sendable, CustomStringConvertible {
     public let value: String
 
+    @inlinable
     public init?(_ value: String) {
         guard !value.isEmpty else { return nil }
         self.value = value
@@ -26,14 +30,17 @@ public struct ContentfulString: Equatable, Hashable, Sendable, CustomStringConve
 
     public var description: String { return value.description }
 
+    @inlinable
     public func prepending(_ string: String) -> ContentfulString {
         return ContentfulString(string + value)!
     }
 
+    @inlinable
     public func appending(_ string: String) -> ContentfulString {
         return ContentfulString(value + string)!
     }
 
+    @inlinable
     public func contains(_ string: String) -> Bool {
         return value.contains(string)
     }
@@ -45,26 +52,34 @@ public struct ContentfulString: Equatable, Hashable, Sendable, CustomStringConve
 }
 
 extension ContentfulString: Comparable {
-    public static func < (lhs: ContentfulString, rhs: ContentfulString) -> Bool {
+    @inlinable
+    public static func < (
+        lhs: ContentfulString,
+        rhs: ContentfulString
+    ) -> Bool {
         return lhs.value < rhs.value
     }
 }
 
 // MARK: Concatenation
 
+@inlinable
 public func + (lhs: ContentfulString, rhs: String) ->
   ContentfulString {
     return lhs.appending(rhs)
 }
 
+@inlinable
 public func + (lhs: String, rhs: ContentfulString) -> ContentfulString {
     return rhs.prepending(lhs)
 }
 
+@inlinable
 public func + (lhs: ContentfulString, rhs: ContentfulString) -> ContentfulString {
     return lhs.appending(rhs.value)
 }
 
+@inlinable
 public func + (lhs: ContentfulString, rhs: ContentfulString?) -> ContentfulString {
     guard let rhs = rhs else { return lhs }
     return lhs + rhs
