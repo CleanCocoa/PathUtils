2de642afa242e58f7ab4fd2858cbc51708e3c886
Christian Tietze <me@christiantietze.de>
2024-11-23 15:19:25 +0100

    inline path appending

--- Sources/PathUtils/Path.swift
+++ Sources/PathUtils/Path.swift
@@ -1,17 +1,23 @@
 //  Copyright © 2022 Christian Tietze. All rights reserved. Distributed under the MIT License.
 
+import Foundation
+
+@inlinable @inline(__always)
 public func + (folder: Folder, filename: Filename) -> String {
     return filename.url(relativeTo: folder, pathExtension: "").absoluteURL.path
 }
 
+@inlinable @inline(__always)
 public func + (folder: Folder, filename: Filename) -> FileURL {
     return folder.fileURL(filename: filename, pathExtension: "")
 }
 
+@inlinable @inline(__always)
 public func + (folder: Folder, basename: Basename) -> String {
     return basename.url(relativeTo: folder).absoluteURL.path
 }
 
+@inlinable @inline(__always)
 public func + (folder: Folder, basename: Basename) -> FileURL {
     return folder.fileURL(basename: basename)
 }
